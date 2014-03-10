3eacf073075361afa8ed5c646e4e8a3e5bab9e2c
root <root@ubuntu.(none)>
2014-03-10 15:33:37 -0700

    Models added

--- news_explorer/models.py
+++ news_explorer/models.py
@@ -1,7 +1,27 @@
 from django.db import models
 
-# Create your models here.
-class LOCATIONDETAIL(models.Model):
+# Create your models here
+
+class LOCATION(models.Model):
     LOCATIONID = models.IntegerField(default=0)
     FILEID = models.IntegerField(default=0)
-    LOCATION = models.CharField(max_length=200)
+    LOCATIONNAME = models.CharField(max_length=200)
+    LOCATIONCOUNT = models.IntegerField(default=0)
+
+class PERSON(models.Model):
+     PERSONID = models.IntegerField(default=0)
+     FILEID = models.IntegerField(default=0)
+     PERSONNAME = models.CharField(max_length=200)
+     PERSONCOUNT = models.IntegerField(default=0)
+
+class ORGANIZATION(models.Model):
+     ORGANIZATIONID = models.IntegerField(default=0)
+     FILEID = models.IntegerField(default=0)
+     ORGANIZATIONNAME = models.IntegerField(default=0)
+     ORGANIZATIONCOUNT = models.IntegerField(default=0)
+
+class FILE(models.Model):
+      FILEID = models.IntegerField(default=0)
+      FILENAME = models.CharField(max_length=200)
+      PATHFILE = models.CharField(max_length=200)
+      PUBLISHEDDATE= models.CharField(max_length=200)
