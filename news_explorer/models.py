0d11ef92759d04e910d2c1f7dec8840d6640b94e
bharat1910 <bharat@Bharats-MacBook-Pro.local>
2014-03-16 23:30:59 -0500

    Changing Parser.py to respect the models

--- news_explorer/models.py
+++ news_explorer/models.py
@@ -2,21 +2,21 @@ from django.db import models
 
 # Create your models here
 class Location(models.Model):
-    id = models.IntegerField(default=0, primary_key=True)
-    field_id = models.IntegerField(default=0)
+    id = models.AutoField(primary_key=True)
+    file_id = models.IntegerField(default=0)
     name = models.CharField(max_length=200)
     count = models.IntegerField(default=0)
 
 class Person(models.Model):
-     id = models.IntegerField(default=0, primary_key=True)
-     field_id = models.IntegerField(default=0)
+     id = models.AutoField(primary_key=True)
+     file_id = models.IntegerField(default=0)
      name = models.CharField(max_length=200)
      count = models.IntegerField(default=0)
 
 class Organization(models.Model):
-     id = models.IntegerField(default=0, primary_key=True)
-     field_id = models.IntegerField(default=0)
-     name = models.IntegerField(default=0)
+     id = models.AutoField(primary_key=True)
+     file_id = models.IntegerField(default=0)
+     name = models.CharField(max_length=200)
      count = models.IntegerField(default=0)
 
 class File(models.Model):
