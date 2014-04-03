89348ee902a25185cb82dafb6d5cc7a983a9d8c1
root <root@ubuntu.(none)>
2014-04-03 01:54:06 -0700

    Json API

--- news_explorer/HttpResponse.py
+++ news_explorer/HttpResponse.py
@@ -1,15 +1,12 @@
-from django.shortcuts import render
-
-# Create your views here.
+__author__ = 'hjdesai2'
+import urllib2
 from django.http import HttpResponse, HttpRequest
 from django.utils import simplejson as Json
 from django.conf import settings
 from django.utils.importlib import import_module
 from news_explorer.models import File, Person, Organization, Location, Article
-
-def index(request):
-    context = {}
-    return render(request, 'news_explorer/index.html', context)
+request = HttpRequest()
+response_data ={}
 
 def jdefault(o):
     if isinstance(o, set):
@@ -43,23 +40,23 @@ def getJson(request):
                                     mimetype="application/json").content
             return response
         elif request.GET["person"]:
-            name = request.GET.get('person', '')
+            name = request.GET['person']
             P = Person.objects.extra(where=['name=%s'], params=[name]).values()
             response = HttpResponse(Json.dumps(str(P), default=jdefault, indent=4), content_type="application/json",
                                     mimetype="application/json").content
             return response
         elif request.GET["organization"]:
-            name = request.GET.get('organization', '')
+            name = request.GET['organization']
             O = Organization.objects.extra(where=['name=%s'], params=[name]).values()
             response = HttpResponse(Json.dumps(str(O), default=jdefault, indent=4), content_type="application/json",
                                     mimetype="application/json").content
             return response
 
-    '''if request.GET["location"] and request.GET["person"] and request.GET["organization"]:
+    if request.GET["location"] and request.GET["person"] and request.GET["organization"]:
         f = File.objects.values()
         response_data = f
         response = HttpResponse(Json.dumps(str(f), default=jdefault, indent=4), content_type="application/json",
-                                mimetype="application/json")
+                                mimetype="application/json").content
         return response
 
     elif request.GET["person"] and request.GET["location"]:
@@ -67,4 +64,8 @@ def getJson(request):
     elif request.GET["organization"] and request.GET["location"]:
         return response
     elif request.GET["organization"] and request.GET["person"]:
-        return response'''
+        return response
+
+
+
+
