181dce0bf1124c75d90093456354257a0d401cd3
root <root@ubuntu.(none)>
2014-04-05 11:04:52 -0700

    Models Updated

--- news_explorer/models.py
+++ news_explorer/models.py
@@ -1,34 +1,36 @@
 from django.db import models
 
 # create your models here
+class File(models.Model):
+    name = models.CharField(max_length=200)
+    path_file = models.CharField(max_length=200)
+    published_date= models.CharField(max_length=200)
+    source= models.CharField(max_length=200)
+    published_location = models.CharField(max_length=20)
+
+class Article(models.Model):
+      file = models.ForeignKey(File)
+      headline = models.CharField(max_length=300)
+      content = models.CharField(max_length=10000)
+      number = models.IntegerField(default=0)
 
 class Location(models.Model):
-    id = models.IntegerField(primary_key=True,default=0)
-    article_id = models.IntegerField(default=0)
+    id = models.IntegerField(primary_key=True, default=0)
+    article = models.ForeignKey(Article)
     name = models.CharField(max_length=200)
     count = models.IntegerField(default=0)
 
 class Person(models.Model):
-     id = models.IntegerField(primary_key=True,default=0)
-     article_id = models.IntegerField(default=0)
+     id = models.IntegerField(primary_key=True, default=0)
+     article = models.ForeignKey(Article)
      name = models.CharField(max_length=200)
      count = models.IntegerField(default=0)
 
 class Organization(models.Model):
-     id = models.IntegerField(primary_key=True,default=0)
-     article_id = models.IntegerField(default=0)
-     name = models.CharField(max_length=200)
-     count = models.IntegerField(default=0)
+    id = models.IntegerField(primary_key=True, default=0)
+    article = models.ForeignKey(Article)
+    name = models.CharField(max_length=200)
+    count = models.IntegerField(default=0)
 
-class File(models.Model):
-      name = models.CharField(max_length=200)
-      path_file = models.CharField(max_length=200)
-      published_date= models.CharField(max_length=200)
-      source= models.CharField(max_length=200)
-      published_location = models.CharField(max_length=20)
 
-class Article(models.Model):
-      file_id = models.IntegerField(default=0)
-      headline = models.CharField(max_length=300)
-      content = models.CharField(max_length=10000)
-      number = models.IntegerField(default=0)
+
