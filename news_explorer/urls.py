517b72aa6bc2f5eb6e575a0dd296704acee1428f
Phoenixs13 <phoenix.s58@gmail.com>
2014-04-05 13:32:29 -0500

    Update urls.py
    Trying out 2 different calls 
    1. to get Drop down list for person,category and organization 
    2. to get Articles for the selection of person,category and organization
    *change required in views.py

--- news_explorer/urls.py
+++ news_explorer/urls.py
@@ -5,5 +5,6 @@ from news_explorer import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^new$', views.new, name='new'),
-    url(r'^initiate$', views.initiate, name='initiate')
+    url(r'^news_explorer/getArticle/(d+)/(d+)/(d+)$', views.initiateArticle, name='initiateArticle'),
+    url(r'^news_explorer/getDropDownList/(d+)$', views.initiateDropDownList, name='initiateDropDownList')
 )
