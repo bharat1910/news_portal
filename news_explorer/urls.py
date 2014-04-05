31ea76fe30965def9defb8bfc0e52ccf16b92b18
Phoenixs13 <phoenix.s58@gmail.com>
2014-04-05 13:48:23 -0500

    Update urls.py

--- news_explorer/urls.py
+++ news_explorer/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^new$', views.new, name='new'),
     url(r'^news_explorer/getArticle/(\d+)/(\d+)/(\d+)$', views.initiateArticle, name='initiateArticle'),
-    url(r'^news_explorer/getDropDownList/(\d+)$', views.initiateDropDownList, name='initiateDropDownList')
+    url(r'^news_explorer/getDropDownList/(\d+)$', views.initiateDropDownList, name='initiateDropDownList'),
+    url(r'^news_explorer/(?P<person>\d+)/(?P<organization>\d+)/(?P<location>\d+)$', views.initiate, name='initiate'),
 )
