790cbadb869c3fe262687ffa1f697d0de93c6eb4
Aditi <me@ubuntu.(none)>
2014-04-12 00:59:49 -0700

    updating urls

--- news_explorer/urls.py
+++ news_explorer/urls.py
@@ -5,5 +5,6 @@ from news_explorer import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^articles$', views.getJson, name='getJson'),
+    url(r'^click_article/$', views.click_article, name='click_article'),
     url(r'^(?P<reqtype>\w+)$', views.initiate_chosen, name='initiate_chosen')
 )
