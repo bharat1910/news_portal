3ebf90d3c881866c5059c100a737f1b0cee751c9
bharat1910 <bharat@Bharats-MacBook-Pro.local>
2014-04-27 19:06:32 -0500

    Removing objectify - not using it

--- news_explorer/views.py
+++ news_explorer/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 import requests
-from lxml import objectify
 from django.http import HttpResponse, HttpRequest
 from django.utils import simplejson as Json
 from django.conf import settings
